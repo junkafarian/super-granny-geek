a21cb1fdc55329fd1b27ead6250f6d6d22468d9d
junkafarian <junkafarian@gmail.com>
2013-02-09 17:14:15 +0000

    Dynamic list loading

--- static/main.js
+++ static/main.js
@@ -1,9 +1,13 @@
 var thefocus = null;
-var lists = [];
+var column_map = {1: 'first', 2: 'second', 3: 'third'};
+var columns = {1: null, 2: null, 3: null};  // 3 columns
+var max_columns = 2; // 0-indexed
 var delta = 0;
 var debug;
 var index = 0;
 var context = 0;
+var products;
+var base_url = '';
 
 var item_module = function() {
 
@@ -14,7 +18,8 @@ items = null;
 
 return {
     init : function(name, column) {
-        node = document.getElementById(name);
+        node = document.getElementById(column_map[column]);
+        node.innerHTML = '';  // wipe existing elements
         jQuery.getJSON('/'+name+'/children/', function(data) {
             items = data;
             for(var i=0; i<data.length; i++) {
@@ -30,8 +35,9 @@ return {
             if (!e) var e = window.event;
             if (e.which) rightclick = (e.which == 3);
             else if (e.button) rightclick = (e.button == 2);
-            alert('Rightclick: ' + rightclick); // true or false
+            return false;
         };
+        columns[column] = this;
 
     },
     setPosition : function(dx) {
@@ -46,10 +52,29 @@ return {
         }
         items[selected].node.style.background = "#999";
 
+    },
+    getSelectedItem : function() {
+        return items[selected];
     }
 };
 
 };
+
+
+function loadProducts(category) {
+    var node = document.getElementById('images');
+    node.innerHTML = '';
+    jQuery.getJSON('/'+category+'/products/', function(data) {
+        products = data;
+        for(var i=0; i<data.length; i++) {
+            var item = document.createElement("div");
+            item.className = "item";
+            item.innerHTML = data[i].name;
+            products[i].node = item;
+            node.appendChild(item);
+        }
+    });
+}
 
 
 function scroll_handler(event) {
@@ -66,7 +91,7 @@ function scroll_handler(event) {
     }
     delta = x;
     debug.innerHTML = "delta" + delta;
-    lists[index].setPosition(delta);
+    columns[index].setPosition(delta);
     return false;
 
 }
@@ -76,9 +101,13 @@ jQuery(document).ready(function () {
 window.oncontextmenu = function() {
     context = 1;
     return false;
-}
+};
 
 window.onclick = function(e) {
+    var current_col,
+        item,
+        column;
+
     if(context == 1) {
         index --;
     } else {
@@ -87,19 +116,30 @@ window.onclick = function(e) {
     if(index < 0) {
         index = 0;
     }
-    if(index >= lists.length) {
-        index = lists.length - 1;
+    if(index > max_columns) {
+        // get the last column
+        index = max_columns;
     }
     context = 0;
     debug.innerHTML = index;
-}
+    current_col = columns[index];
+    item = current_col.getSelectedItem();
+
+    if (index < max_columns) {
+        // load the next column
+        column = item_module().init(item.slug, index + 1);
+    }
+
+    // load the products for this category
+    loadProducts(item.slug);
+
+};
 
 
 debug = document.getElementById("debug");
 
-root = item_module().init('root');
-lists.push(root);
-
+root = item_module().init('root', 1);
+loadProducts('root');
 
 document.onmousewheel = scroll_handler;
 
