3363d66116c55f7d303b68dca32e66acf210d6e1
junkafarian <junkafarian@gmail.com>
2013-02-09 15:44:56 +0000

    tabs -> spaces

--- static/main.js
+++ static/main.js
@@ -11,46 +11,46 @@ position = 0,
 items = null;
 
 return {
-	init : function(name) {
-		node = document.getElementById(name);
-		jQuery.getJSON('static/'+name+'.json', function(data) {
-			items = data;
-			  for(var i=0; i<data.length; i++) {
-			  	var item = document.createElement("div");
-			  	item.className = "item";
-			  	item.innerHTML = data[i].title;
-			  	// item.onmouseover = function() {
-			  	// 	this.style.background = "#eee";
-			  	// };
-			  	// item.onmouseout = function() {
-			  	// 	this.style.background = "#ddd";
-			  	// };
-			  	items[i].node = item;
-			  	node.appendChild(item);
-			  }
-		});
-		node.onclick = function(e) {
-			var rightclick;
-			if (!e) var e = window.event;
-			if (e.which) rightclick = (e.which == 3);
-			else if (e.button) rightclick = (e.button == 2);
-			alert('Rightclick: ' + rightclick); // true or false
-			}
+    init : function(name) {
+        node = document.getElementById(name);
+        jQuery.getJSON('static/'+name+'.json', function(data) {
+            items = data;
+              for(var i=0; i<data.length; i++) {
+                var item = document.createElement("div");
+                item.className = "item";
+                item.innerHTML = data[i].title;
+                // item.onmouseover = function() {
+                //  this.style.background = "#eee";
+                // };
+                // item.onmouseout = function() {
+                //  this.style.background = "#ddd";
+                // };
+                items[i].node = item;
+                node.appendChild(item);
+              }
+        });
+        node.onclick = function(e) {
+            var rightclick;
+            if (!e) var e = window.event;
+            if (e.which) rightclick = (e.which == 3);
+            else if (e.button) rightclick = (e.button == 2);
+            alert('Rightclick: ' + rightclick); // true or false
+            }
 
-	},
-	setPosition : function(dx) {
-		position += dx;
-		items[selected].node.style.background = "#eee";
-		selected = Math.floor(position/50);
-		if(selected >= items.length) {
-			selected = items.length - 1;
-		} else if (selected < 0) {
+    },
+    setPosition : function(dx) {
+        position += dx;
+        items[selected].node.style.background = "#eee";
+        selected = Math.floor(position/50);
+        if(selected >= items.length) {
+            selected = items.length - 1;
+        } else if (selected < 0) {
 
-			selected = 0;
-		}
-		items[selected].node.style.background = "#999";
+            selected = 0;
+        }
+        items[selected].node.style.background = "#999";
 
-	}
+    }
 };
 
 };
@@ -58,28 +58,28 @@ return {
 
 function scroll_handler(event) {
 
-	var x;
+    var x;
     if (!event) event = window.event;
     // normalize the delta
     if (event.wheelDelta) {
         // IE and Opera
-			x = event.wheelDelta / 30;
+            x = event.wheelDelta / 30;
     } else if (event.detail) {
         // W3C
-        	x = event.detail;
+            x = event.detail;
     }
-    	delta = x;
-	debug.innerHTML = "delta" + delta;
-	lists[0].setPosition(delta);
-		preventDefault(event);
+        delta = x;
+    debug.innerHTML = "delta" + delta;
+    lists[0].setPosition(delta);
+        preventDefault(event);
 
 }
 
 jQuery(document).ready(function () {
 
 window.oncontextmenu = function() {
-	//alert("cointex");
-	return false;
+    //alert("cointex");
+    return false;
 }
 
 debug = document.getElementById("debug");
